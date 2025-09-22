57bd56cbcd22eb0f160df025bd48cee6c98751c3
Dolmatov Daniil <dandolmatov@Dolmatovs-MacBook-Air.local>
2025-09-22 20:21:02 +0300

    add runner

---
+++ itmo_tests/run.py
@@ -0,0 +1,42 @@
+import subprocess
+import csv
+
+path_to_exe = "../../mathcs-ca-25-floating-point-dolmatovDan/main"
+
+with open("true_gen_float_mult_tests.tsv", 'r', encoding='utf-8') as file:
+    tsv_file = csv.reader(file, delimiter=",")
+    passed = []
+    failed = []
+    error = []
+    for line in tsv_file:
+        if len(line) != 2:
+            continue
+        arr = line[0].split()
+        if len(arr) == 5:
+            if arr[3] == '*':
+                arr[3] = "\\*"
+            correct = " ".join([arr[0], arr[1], arr[3], arr[2], arr[4]])
+        else:
+            correct = line[0]
+        process = subprocess.run(f"{path_to_exe} {correct}", shell=True, capture_output=True, text=True)
+        stdout = process.stdout
+        stderr = process.stderr
+
+        print("--------------------")
+        print(f"Test: {line[0]}", end="")
+        if stderr:
+            print(f"\n\033[91mError: {stderr}\033[0m")
+            error.append(f"{line[0]}")
+            continue
+        if stdout == line[1]:
+            print(f" \033[92mPass\033[0m")
+            print(f"Output: {stdout}")
+            passed.append(f"{line[0]}")
+        else:
+            print(f" \033[91mFail\033[0m")
+            print(f"Anwser: {line[1]}")
+            print(f"Output: {stdout}")
+            failed.append(f"{line[0]}")
+    print("Passed:", len(passed), "tests; failed", len(failed), "tests; error in", len(error), "tests.")
+input("\nPress Enter to continue...")
+
